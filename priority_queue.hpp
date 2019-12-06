c6f2972a6a49e921067b175d56eb85964bcee9d3
Hunter Meredith <hunterjmery@gmail.com>
2019-12-05 21:59:45 -0700

    uhh, no erros with first part but it doesn't work

--- priority_queue.hpp
+++ priority_queue.hpp
@@ -1,3 +1,5 @@
+#pragma once
+#include <cmath>
 #include <iostream>
 #include <vector>
 
@@ -54,7 +56,6 @@ namespace usu
             return priority;
         }
 
-      //private:
         T value;
         T1 priority;
     };
@@ -63,51 +64,162 @@ namespace usu
     class priority_queue
     {
       public:
-        //am I doing this right? there's no way it's that simple, but if so, why do we do it?
         using priority_type = T1;
         using value_type = T;
         using size_type = size_t;
 
-        void enqueue(T value, priority_type priority)
+        //If the node is a leaf node
+        //Def works if given the count and not the exact index. AKA give it index + 1
+        bool isLeaf(int pos)
         {
-            nextNode = pqNode(value, priority);
-            //Is this where I should use the formula new size = (current size) * 1.25 + 1 ?
-            //Why not just push back?
-            queue.push_back(nextNode);
+            bool returnVal = pos > countNodes / 2 && pos <= countNodes;
+            return returnVal;
         }
 
-        auto dequeue()
+        // Return position of parent given child
+        //Def works if given the count and not the exact index. AKA give it index + 1
+        int parent(int pos)
         {
-            if (this->empty())
+            if (pos <= 0)
+                return -1;
+            pos = static_cast<double>(pos);
+            return std::round((pos - 1.0) / 2.0);
+        }
+
+        // Return position for left child of pos
+        //Def works if given the count and not the exact index. AKA give it index + 1
+        int leftchild(int pos)
+        {
+            return 2 * pos;
+        }
+
+        // Return position for right child of pos
+        //Def works if given the count and not the exact index. AKA give it index + 1
+        int rightchild(int pos)
+        {
+            return 2 * pos + 1;
+        }
+
+        // Put element in its correct place
+        void siftdown(int pos)
+        {
+            while (!isLeaf(pos))
+            {
+                //set a current node and its index
+                //find it's largest child
+                //swap the current node with its child
+                //move to that node
+                int j;
+                if (rightchild(pos).priority > leftchild(pos).priority)
+                {
+                    j = rightchild(pos);
+                }
+                else
+                {
+                    j = leftchild(pos);
+                }
+
+                if (queue[pos].priority < queue[j].priority)
+                {
+                    pqNode<T, T1> temp = queue[pos];
+                    queue[pos] = queue[j];
+                    queue[j] = temp;
+                    pos = j;
+                }
+                else
+                {
+                    pos = j;
+                }
+            }
+        }
+
+        // Put element in its correct place
+        void siftup(int pos)
+        {
+            while (pos != 0)
+            {
+                //set a current node and its index
+                //swap the current node with its parent if it's larger
+                //move to that node
+                int j = parent(pos);
+                if (queue[pos].priority > queue[j].priority)
+                {
+                    pqNode<T, T1> temp = queue[pos];
+                    queue[pos] = queue[j];
+                    queue[j] = temp;
+                    pos = j;
+                }
+                else
+                {
+                    pos = j;
+                }
+            }
+        }
+
+        void enqueue(T value, priority_type priority)
+        {
+            pqNode<T, T1> nextNode = pqNode<T, T1>(value, priority);
+            queue.push_back(nextNode);
+            queue[countNodes] = nextNode;
+            if (countNodes == 0)
+            {
+                ++countNodes;
+                return;
+            }
+            int parentPos = parent(countNodes);
+            // Now sift up until curr's parent's key > curr's key
+            while (countNodes != 0 && queue[countNodes].priority > queue[parentPos].priority)
+            {
+                pqNode<T, T1> temp = queue[parent(countNodes)];
+                queue[parent(countNodes)] = queue[countNodes];
+                queue[countNodes] = temp;
+            }
+            ++countNodes;
+            //Is this where I should use the formula new size = (current size) * 1.25 + 1 ?
+            //Why not just push back?
+        }
+
+        pqNode<T, T1> dequeue()
+        {
+            if (empty())
             {
                 throw "The queue is empty!";
             }
             else
             {
-                //copy this->queue.begin() into into an iterator-How??
-                //this->queue.erase(this->queue.begin());
-                //this->update();
-                //return copy from first line
+                //When i do the decrement first it accesses the real last element and not one past the end
+                --countNodes;
+                //swap first and last element
+                pqNode<T, T1> temp = queue[0];
+                queue[0] = queue[countNodes];
+                queue[countNodes] = temp;
+                //copy last element
+                pqNode<T, T1> copy = queue[countNodes];
+                //delete last element
+                queue.pop_back();
+                siftdown(0);
+                return copy;
             }
         }
 
-        iterator find(T value)
+        //replace int with iterator
+        int find(T value)
         {
-            for (int i = 0; i < this->size(); i++)
+            for (int i = 0; i < countNodes; i++)
             {
-                if (queue[i].getValue() == value)
+                if (queue[i].value == value)
                 {
-                    //return iterator to value
+                    return queue[i].value;
                 }
             }
         }
 
         //Ask if this is the right idea for update
-        void update(iterator i, priority_type priority)
+        //Replace int in parameters with iterator
+        void update(int i, priority_type priority)
         {
             //Sort vector according to priorities with highest priority as element 0
             //how to use the binary predicate to sort based on priorities
-            sort(queue.begin(), queue.end(), wayToSort());
         }
 
         bool empty()
@@ -124,10 +236,11 @@ namespace usu
 
         size_type size()
         {
-            return queue.size();
+            return countNodes;
         }
 
       private:
-        std::vector<pqNode> queue;
+        std::vector<pqNode<T, T1>> queue;
+        int countNodes;
     };
 } // namespace usu
